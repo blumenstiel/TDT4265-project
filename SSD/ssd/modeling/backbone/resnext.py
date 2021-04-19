f81f1010b120494e46d9625bc8aced1085a20746
Hauke Thomsen <hauke.thomsen@delta-ev.de>
2021-04-19 11:18:51 +0200

    Small fixes

--- SSD/ssd/modeling/backbone/resnext.py
+++ SSD/ssd/modeling/backbone/resnext.py
@@ -3,13 +3,11 @@ import torchvision.models as models
 from torch import nn
 
 
-#cfg.MODEL.BACKBONE.PRETRAINED
-
 class Resnext():
     def __init__(self, cfg):
-        self.model = models.resnext50_32x4d(pretrained=True, progress=True)
+        self.model = models.resnext50_32x4d(pretrained=cfg.MODEL.BACKBONE.PRETRAINED, progress=True)
 
-        self.add_lay1 = nn.Sequential(
+        self.add_lay5 = nn.Sequential(
             nn.Conv2d(in_channels=2048, out_channels=1024, kernel_size=1, stride=1),
             nn.BatchNorm2d(1024, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, padding=1, stride=2),
@@ -20,7 +18,7 @@ class Resnext():
             nn.BatchNorm2d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU(inplace=True),
         )
-        self.add_lay2 = nn.Sequential(
+        self.add_lay6 = nn.Sequential(
             nn.Conv2d(in_channels=2048, out_channels=1024, kernel_size=1, stride=1),
             nn.BatchNorm2d(1024, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, padding=1, stride=2),
@@ -31,7 +29,7 @@ class Resnext():
             nn.BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU(inplace=True),
         )
-        self.add_lay3 = nn.Sequential(
+        self.add_lay7 = nn.Sequential(
             nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, stride=1),
             nn.BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1, stride=2),
@@ -56,11 +54,11 @@ class Resnext():
         out_features.append(x)
         x = self.model.layer4(x)
         out_features.append(x)
-        x = self.add_lay1(x)
+        x = self.add_lay5(x)
         out_features.append(x)
-        x = self.add_lay2(x)
+        x = self.add_lay6(x)
         out_features.append(x)
-        x = self.add_lay3(x)
+        x = self.add_lay7(x)
         out_features.append(x)
 
         #for i in out_features:
@@ -69,6 +67,6 @@ class Resnext():
         return tuple(out_features)
 
 
-model = Resnext('_')
-model.forward(torch.zeros((1, 3, 300, 300)))
+#model = Resnext('_')
+#model.forward(torch.zeros((1, 3, 300, 300)))
 
