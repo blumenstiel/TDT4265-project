c8fe9f4cc6ed7971897fa8372999149255a36d75
Benedikt Blumenstiel <benedikt.blumenstiel@kit.edu>
2021-04-22 00:49:04 +0200

    Fix RandomSampleCrop

--- SSD/ssd/data/transforms/transforms.py
+++ SSD/ssd/data/transforms/transforms.py
@@ -156,7 +156,8 @@ class RandomSampleCrop(object):
             (None, None),
         )
         # random crop to target image_size
-        self.image_ratio = image_size[1] / image_size[0]
+        if image_size:
+            self.image_ratio = image_size[1] / image_size[0]
 
     def __call__(self, image, boxes=None, labels=None):
         # guard against no boxes
